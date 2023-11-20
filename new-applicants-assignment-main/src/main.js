b772ba3c053b68ee7d5ea125f113401448b69e27
Hammad1820 <48063834+Hammad1820@users.noreply.github.com>
2023-11-20 19:21:45 +0100

    Add files via upload

---
+++ new-applicants-assignment-main/src/main.js
@@ -0,0 +1,17 @@
+// Add your JavaScript here
+// const display= document.querySelector('.display');
+
+// let input = "";
+// for (let value of values) {
+
+//     const value = display;
+
+//     display.addEventListener('click',() => {
+//         if(value=='clear'){
+
+//             display="";
+         
+
+//         } 
+//     })
+// }
